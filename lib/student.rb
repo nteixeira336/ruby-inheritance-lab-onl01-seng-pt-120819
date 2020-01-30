0465a6b64c8de8112badcb98acdb03c2189d951b
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-01-30 16:10:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User 
+attr_accessor :knowledge 
 
+def initialize 
+  @knowledge= []
+end   
+
+def learn ("Ruby framework Rails gem bundle update")
+  @knowlege << "Ruby framework Rails gem bundle update"
 end
+
+end 
